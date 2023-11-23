df43700085947f9ce8b69d9f6b1fd0e9016cecb4
ani-ruddha <aniruddhadas919@gmail.com>
2023-11-23 21:42:22 +0530

    update

--- src/test/java/sampleExecution/PropertiesExecutiontest.java
+++ src/test/java/sampleExecution/PropertiesExecutiontest.java
@@ -15,6 +15,7 @@ public class PropertiesExecutiontest {
 		String URL = PUTIL.getDataFromProperties("url");
 		System.out.println("welcome");
 		System.out.println("most welcome");
+		System.out.println("abcxyz");
 		
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
