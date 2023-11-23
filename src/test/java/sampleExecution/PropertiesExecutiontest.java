44bdab060cd2a05b4bf98883d4ebb88980ce48b1
ani-ruddha <aniruddhadas919@gmail.com>
2023-11-23 12:21:09 +0530

    2nd commit for pollscm

--- src/test/java/sampleExecution/PropertiesExecutiontest.java
+++ src/test/java/sampleExecution/PropertiesExecutiontest.java
@@ -13,6 +13,7 @@ public class PropertiesExecutiontest {
 		String USERNAME =PUTIL.getDataFromProperties("username");
 		String PASSWORD = PUTIL.getDataFromProperties("password");
 		String URL = PUTIL.getDataFromProperties("url");
+		System.out.println("welcome");
 		
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
